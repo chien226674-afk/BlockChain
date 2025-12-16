e82f8dd50afe8a7e8d7861c7e79d6a4a492c6ffe
Chien Minh <chien226674@student.nctu.edu.vn>
2025-12-16 14:40:13 +0700

    hoan thanh cau hinh

---
+++ blockchain_ui/src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import {Button} from "@/components/ui/button"
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+    <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default App
