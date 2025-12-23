594706ee498ea85239dad916ac6dc632e00c783d
Chien Minh <chien226674@student.nctu.edu.vn>
2025-12-23 15:36:30 +0700

    Done icon header ui

--- blockchain_ui/src/components/ui/button.tsx
+++ blockchain_ui/src/components/ui/button.tsx
@@ -10,14 +10,16 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        primary: "bg-primary text-primary-foreground cursor-pointer pt-6",
+         ghost:
+          "text-bold text-[#ffffff] text-[1rem] mr-[20px] transform transition duration-400 hover:scale-95 cursor-pointer ",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
           "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost:
-          "text-[#ffffff] ",
+       
         link: "text-primary underline-offset-4 hover:underline",
         
       },
