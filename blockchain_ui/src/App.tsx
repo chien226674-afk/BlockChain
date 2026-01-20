b9904475b378aba398e9cf30675ec41501707575
Chien Minh <chien226674@student.nctu.edu.vn>
2026-01-20 12:50:37 +0700

    add NotFound pages

--- blockchain_ui/src/App.tsx
+++ blockchain_ui/src/App.tsx
@@ -1,14 +1,19 @@
 // import { useState } from 'react'
-import {Button} from "@/components/ui/button"
+// import {Button} from "@/components/ui/button"
 import './App.css'
+import { RouterProvider } from "react-router-dom"
+import router from "./router"
 
 function App() {
   
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-    <Button>Click Me</Button>
-    </div>
+    // <div className="flex flex-col items-center justify-center min-h-svh">
+    // <Button>Click Me</Button>
+    // </div>
+    <>
+    <RouterProvider router={router} />
+    </>
   )
 }
 
